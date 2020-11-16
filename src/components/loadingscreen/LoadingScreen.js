25d531e4d675bc9a35f854432b4a52007a8c5390
Valentine <32327721+VVValx@users.noreply.github.com>
2020-11-16 09:38:34 +0200

    added spinning circle

--- src/components/loadingscreen/LoadingScreen.js
+++ src/components/loadingscreen/LoadingScreen.js
@@ -4,17 +4,15 @@ import load from "./loadingscreen.module.css";
 function LoadingScreen() {
   return (
     <div className={load.container}>
-      <div className={load.circleContainer}>
-        <div className={load.loadType}>
-          <div className={`${load.circle} ${load.circle1}`}></div>
-          <div className={`${load.circle} ${load.circle2}`}></div>
-          <div className={`${load.circle} ${load.circle3}`}></div>
-          <div className={`${load.circle} ${load.circle4}`}></div>
-        </div>
+      <div className={load.loadType}>
+        <div className={`${load.circle} ${load.circle1}`}></div>
+        <div className={`${load.circle} ${load.circle2}`}></div>
+        <div className={`${load.circle} ${load.circle3}`}></div>
+        <div className={`${load.circle} ${load.circle4}`}></div>
+      </div>
 
-        <div className={load.loadType}>
-          <div className={load.spinTop}></div>
-        </div>
+      <div className={load.loadType}>
+        <div className={load.spin}></div>
       </div>
     </div>
   );
